5aba63d4233b27ad21a93528bfc125d85682c647
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-07-29 11:40:21 +0200

    Identify timed EFF stream constructor tables

--- tools/reccmp_ilt_compat.py
+++ tools/reccmp_ilt_compat.py
@@ -70,6 +70,11 @@ FUNCTION_DATA_REFERENCES = {
         "g_EFF_NetworkLobbyPeerDirtyConfirmVtable",
         "network lobby peer dirty-confirm vtable 004985d8",
     ),
+    0x0045FCE0: (
+        0x00498FB0,
+        "vftable'{for `VsNetTimedEffStreamPrimary",
+        "timed EFF stream primary vtable 00498fb0",
+    ),
     0x0046DE70: (
         0x00499F30,
         "g_VSGDI_AnimWindowOwnerVtable",
