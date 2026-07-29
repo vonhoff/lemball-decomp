a24179b0f998f1bfbba7064fe7130f2c14d32033
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-07-29 14:58:01 +0200

    Correlate slot manager destructor vtable

--- tools/reccmp_ilt_compat.py
+++ tools/reccmp_ilt_compat.py
@@ -80,6 +80,11 @@ FUNCTION_DATA_REFERENCES = {
         "vftable'{for `VsNetTimedEffStreamPrimary",
         "timed EFF stream primary vtable 00498fb0",
     ),
+    0x0046BC90: (
+        0x00499D78,
+        "g_VSGDI_ResourceGeometryHelperSlotManagerVtable",
+        "resource geometry helper slot-manager vtable 00499d78",
+    ),
     0x0046DE70: (
         0x00499F30,
         "g_VSGDI_AnimWindowOwnerVtable",
