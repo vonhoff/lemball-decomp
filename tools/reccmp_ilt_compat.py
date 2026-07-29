d49a1d2d5862442dfa39b4beb1b0db9d0eb4bb0e
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-07-29 10:25:14 +0200

    Recover managed entity packet 27 class

--- tools/reccmp_ilt_compat.py
+++ tools/reccmp_ilt_compat.py
@@ -21,10 +21,13 @@ VTABLE_METHOD_ILTS = {
     0x004018C5,  # ManagedEntityStateResetPacket scalar deleting destructor
     0x00401C94,  # ManagedEntityPacket25::WriteEffStreamTaggedHeader
     0x0040217B,  # ManagedEntityPacketBase::ReverseEffStreamPayload
+    0x00402338,  # ManagedEntityPacket27::ReverseEffStreamPayload
     0x004025B3,  # ManagedEntityPacket26::ReverseEffStreamPayload
     0x004026AD,  # VsNetEffStreamCommon::AcceptEffStream
+    0x00402CE3,  # ManagedEntityPacket27::WriteEffStreamTaggedHeader
     0x00402DB0,  # ManagedEntityPacket26 scalar deleting destructor
     0x00402E41,  # ManagedEntityPacket26::WriteEffStreamTaggedHeader
+    0x0040308F,  # ManagedEntityPacket27 scalar deleting destructor
     0x004030E4,  # ManagedEntityPacketBase scalar deleting destructor
     0x00403288,  # ManagedEntityPacket2A scalar deleting destructor
 }
