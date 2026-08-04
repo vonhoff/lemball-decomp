ab8d49bf55d20d861a4eefb1418ee38800201098
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-08-04 03:18:21 +0200

    Restore balloon activation ownership

--- src/AI/CBalloon.h
+++ src/AI/CBalloon.h
@@ -3,7 +3,11 @@
 
 class CBalloon {
 public:
-	char m_abReserved00[0x94];
+	char m_abReserved00[0x5c];
+	void* m_pTarget5C;
+	char m_abReserved60[4];
+	int m_nObjectType64;
+	char m_abReserved68[0x2c];
 	int m_nFrameTime94;
 	char m_abReserved98[4];
 	int m_nPositionX9C;
@@ -11,6 +15,7 @@ public:
 	int m_nPositionZA4;
 
 	void Restart(void);
+	int Activate(void* pTarget);
 	int* ActivatePosition(int* pPoint);
 };
 
