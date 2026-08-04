f1ce8212946867a6ff98e94a3933248d72161360
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-08-04 06:44:11 +0200

    Extract generic group from legacy files

--- src/AI/CEnemyGrp.cpp
+++ src/AI/CEnemyGrp.cpp
@@ -1,62 +1,20 @@
 #include "Platform/Windows/Mixed/Engine/CORE/WIN32.H"
+#include "AI/CGenericGroup.h"
 
 struct LevelChunkObjectBaseView {
 	void* InitializeLevelChunkObjectBase(int nType, unsigned short nVariant, unsigned short nStateId);
 };
 
-struct EnmyManagedEntityGroupMemberView {};
-struct CompositeLevelChunkChildMemberView {};
-
-typedef void (EnmyManagedEntityGroupMemberView::*RequestStateProc)(int nStateId);
-typedef void (CompositeLevelChunkChildMemberView::*ServiceChildProc)(void);
-
-struct EnmyManagedEntityGroupVtableView {
-	void* m_apReserved00[78];
-	RequestStateProc m_pRequestState138;
-};
-
-struct CompositeLevelChunkChildVtableView {
-	void* m_apReserved00[5];
-	ServiceChildProc m_pService14;
-};
-
-struct CompositeLevelChunkChildView {
-	CompositeLevelChunkChildVtableView* m_pVtable00;
-};
-
 struct EnmyManagedEntityGroupView : LevelChunkObjectBaseView {
-	EnmyManagedEntityGroupVtableView* m_pVtable00;
+	void* m_pVtable00;
 	char m_abReserved04[0x120];
-	int m_cChildren124;
-	CompositeLevelChunkChildView* m_apChildren128[10];
 
 	EnmyManagedEntityGroupView* ConstructEnmyManagedEntityGroup(int nType,
 																unsigned short nVariant,
 																unsigned short nStateId);
 };
 
-extern void* g_LEVELVT_EnmyManagedEntityGroupVtable[16];
-
-// MACINTOSH: CGenericGroup::Process()
-// FUNCTION: LEMBALL 0x0041deb0
-int LEMBALL_FASTCALL ServiceEnmyManagedEntityGroup(EnmyManagedEntityGroupView* pGroup)
-{
-	CompositeLevelChunkChildView** ppChild;
-	CompositeLevelChunkChildView* pChild;
-	int iChild;
-
-	iChild = 0;
-	(((EnmyManagedEntityGroupMemberView*) pGroup)->*pGroup->m_pVtable00->m_pRequestState138)(0x18);
-	if (pGroup->m_cChildren124 > 0) {
-		ppChild = pGroup->m_apChildren128;
-		do {
-			pChild = *ppChild++;
-			++iChild;
-			(((CompositeLevelChunkChildMemberView*) pChild)->*pChild->m_pVtable00->m_pService14)();
-		} while (iChild < pGroup->m_cChildren124);
-	}
-	return 0;
-}
+extern CGenericGroupChunkVtable g_LEVELVT_EnmyManagedEntityGroupVtable;
 
 struct CEnemyGroup : EnmyManagedEntityGroupView {
 	CEnemyGroup(int nAI, unsigned short nObjectManager, unsigned short nFormationManager);
@@ -66,5 +24,5 @@ struct CEnemyGroup : EnmyManagedEntityGroupView {
 CEnemyGroup::CEnemyGroup(int nAI, unsigned short nObjectManager, unsigned short nFormationManager)
 {
 	InitializeLevelChunkObjectBase(nAI, nObjectManager, nFormationManager);
-	*(void**) this = g_LEVELVT_EnmyManagedEntityGroupVtable;
+	*(void**) this = &g_LEVELVT_EnmyManagedEntityGroupVtable;
 }
---
+++ src/AI/CGenericGroup.h
@@ -0,0 +1,75 @@
+#ifndef LEMBALL_CGENERICGROUP_H
+#define LEMBALL_CGENERICGROUP_H
+
+#include "AI/CGameObject.h"
+
+class CFormationManager;
+class CViewData;
+struct CGenericGroupVtable;
+
+class CGenericGroup {
+public:
+	CGenericGroupVtable* m_pVtable00;
+	char m_abReserved04[0x120];
+	int m_nElementCount124;
+	CGameObject* m_apElements128[10];
+	int m_nCurrentElement150;
+	char m_abReserved154[4];
+	CVSRect m_BoundingBox158;
+
+	void Restart(void);
+	int Process(void);
+	int GetElementsInGroup(void);
+	CGameObject* GetFirstElementInGroup(void);
+	CGameObject* GetNextElementInGroup(void);
+	CGameObject* GetCurrentElementInGroup(void);
+	CGameObject* GetNthElementInGroup(int nElement);
+	void SwapElements(CGameObject* pFirst, CGameObject* pSecond);
+	void AddElementToGroup(CGameObject* pObject);
+	void RemoveElementFromGroup(CGameObject* pObject);
+	int ConfirmElementIsInGroup(CGameObject* pObject);
+	int ConfirmElementIsInGroup(unsigned short nId);
+	CVSRect GetBoundingBox(void);
+	void GetBoundingBox(CVSRect& rect);
+	void CalculateBoundingBox(int nRadius);
+	void AddNewWaypoint(AICOORD position, CFormationManager* pFormationManager);
+	void SendNewWaypoint(AICOORD position);
+	void OverideExistingWaypoints(AICOORD position);
+	void ClearExistingWaypoints(void);
+	void SetFormationIndex(int nIndex);
+	int GetFormationIndex(void);
+	int CheckGroupIntersection(CVSRect* pRect, AICOORD* pPosition);
+	int GetViewData(CViewData* pViewData);
+};
+
+typedef int (CGenericGroup::*GenericGroupContainsProc)(CGameObject* pObject);
+typedef void (CGenericGroup::*GenericGroupDeleteProc)(int fFree);
+typedef int (CGenericGroup::*GenericGroupCountProc)(void);
+typedef void (CGenericGroup::*GenericGroupElementProc)(CGameObject* pObject);
+typedef void (CGenericGroup::*GenericGroupBoundsProc)(short* pBounds);
+typedef int (CGenericGroup::*GenericGroupViewDataProc)(CViewData* pViewData);
+typedef int (CGenericGroup::*GenericGroupIntersectionProc)(CVSRect* pRect, AICOORD* pPosition);
+
+struct CGenericGroupChunkVtable {
+	void* m_apSlots00[5];
+	int (CGenericGroup::*m_pProcess14)(void);
+	void* m_apSlots18[10];
+};
+
+struct CGenericGroupVtable {
+	GenericGroupDeleteProc m_pDelete00;
+	void* m_apReserved04[17];
+	GenericGroupBoundsProc m_pGetBoundingBox48;
+	void* m_apReserved4C[47];
+	GenericGroupCountProc m_pGetNumberOfElements108;
+	void* m_apReserved10C[6];
+	GenericGroupElementProc m_pAddElement124;
+	GenericGroupElementProc m_pRemoveElement128;
+	void* m_pReserved12C;
+	GenericGroupContainsProc m_pContains130;
+	void* m_apReserved134[9];
+	GenericGroupViewDataProc m_pGetViewData158;
+	GenericGroupIntersectionProc m_pCheckIntersection15C;
+};
+
+#endif
