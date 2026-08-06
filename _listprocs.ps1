261c198fc7b7487c7dbc0f6bde284972e2881509
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-08-06 15:35:30 +0200

    Revert "legacy: lowercase LVTRIPLET.CPP->lvtriplet.cpp, LVGMOBPT.CPP->lvgmobpt.cpp (all-caps extension migration); update CMakeLists; byte-safe, 2948 implemented, gate 0/0, 0x4132c0 still 100%"
    This reverts commit 3d9776e13e2024ddb500ab5de0061e1e7142a6a3.

--- _listprocs.ps1
+++
@@ -1,3 +0,0 @@
-Get-CimInstance Win32_Process | Where-Object {
-  $_.CommandLine -match 'hermes_cli.main|gateway run|hermes serve|Hermes.exe'
-} | ForEach-Object { "PID=$($_.ProcessId) PP=$($_.ParentProcessId) NAME=$($_.Name)`n$($_.CommandLine)`n----" }
