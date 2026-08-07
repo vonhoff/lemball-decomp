09c9927ee99e1f19fc947adbc0b5f7c954cd4b6f
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-08-07 18:52:27 +0200

    rehome: service_composite_children_and_seek_target (0x41f820, 44.1% - vtable request + child-serve loop + 2 seek helpers). Unc:163 of 528. lint clean

--- src/Control/ClusterScanOps2.cpp
+++ src/Control/ClusterScanOps2.cpp
@@ -1015,6 +1015,24 @@ int __fastcall CoalesceFreeMemoryBlockWithTrailingNeighbor(void* pThis, int nUnu
 	return 0;
 }
 
+// MACINTOSH: service_composite_children_and_seek_target()
+// FUNCTION: LEMBALL 0x0041f820
+int __fastcall ServiceCompositeChildrenAndSeekTarget(void* pObject)
+{
+	int iVar3 = 0;
+	(*( void(**)(int)) (*(void***) pObject + 0x138 / 4))(0x18);
+	if (*(int*) ((char*) pObject + 0x124) > 0) {
+		int i;
+		for (i = 0; i < *(int*) ((char*) pObject + 0x124); i++) {
+			void* piVar1 = *(void**) ((char*) pObject + 0x128 + i * 4);
+			(*( void(**)(void)) (*(void***) piVar1 + 0x14 / 4))();
+		}
+	}
+	((void(__fastcall*)(void*)) 0x403765)(pObject);
+	((void(__fastcall*)(void*)) 0x402577)(pObject);
+	return 0;
+}
+
 // MACINTOSH: award_300_points_mark_flag_and_emit_callback()
 // FUNCTION: LEMBALL 0x004206a0
 void __fastcall Award300PointsMarkFlagAndEmitCallback(void* pObject)
