19df0a68075bcdbbd0b0360d51b78a1027c2c1cd
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-08-07 16:11:24 +0200

    rehome: reconstruct remove_queued_projectile_request_code (0x40fc50, 80.7% - scan+shift-down array remove). Unc:54 of 528. lint clean

--- src/Control/ClusterScanOps.cpp
+++ src/Control/ClusterScanOps.cpp
@@ -43,3 +43,29 @@ int __fastcall GetLastQueuedProjectileRequestCode(void* pObject)
 	}
 	return 0xffff;
 }
+
+// MACINTOSH: remove_queued_projectile_request_code(int)
+// FUNCTION: LEMBALL 0x0040fc50
+void __fastcall RemoveQueuedProjectileRequestCode(void* pObject, int nUnused, int param_1)
+{
+	int iVar1 = *(int*) ((char*) pObject + 0x220);
+	int iVar4 = 0;
+	if (iVar1 > 0) {
+		int* piVar3 = (int*) ((char*) pObject + 0x1c0);
+		while (piVar3[0] != param_1) {
+			piVar3++;
+			iVar4++;
+			if (iVar1 <= iVar4) {
+				return;
+			}
+		}
+		iVar4++;
+		if (iVar4 < iVar1) {
+			do {
+				*(int*) ((char*) pObject + iVar4 * 4 + 0x1bc) = *(int*) ((char*) pObject + (iVar4 + 1) * 4 + 0x1bc);
+				iVar4++;
+			} while (iVar4 < *(int*) ((char*) pObject + 0x220));
+		}
+		*(int*) ((char*) pObject + 0x220) = *(int*) ((char*) pObject + 0x220) - 1;
+	}
+}
