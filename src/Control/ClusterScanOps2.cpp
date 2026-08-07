a77e44d87cfc1d5bccc5133024c4431c6cfac25e
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-08-07 20:06:11 +0200

    rehome (batch): append_managed_entity_group_from_ids (0x41ebe0, 60% - alloc+construct+registry-id group-add) + destroy_level_chunk_stream_dispatcher (0x40b0d0, 54.2% - vtable-restore multi-release dtor). Unc:186-187 of 528. lint clean

--- src/Control/ClusterScanOps2.cpp
+++ src/Control/ClusterScanOps2.cpp
@@ -1615,3 +1615,30 @@ int __fastcall LoadListEntryPairFromStream(void* pObject, int nUnused, int param
 	}
 	return 1;
 }
+
+// MACINTOSH: append_managed_entity_group_from_ids(unsigned short, unsigned short*)
+// FUNCTION: LEMBALL 0x0041ebe0
+void __fastcall AppendManagedEntityGroupFromIds(void* pObject, int nUnused, unsigned short nIdCount, unsigned short* pIdList)
+{
+	void* pMsg;
+	void* pGroup;
+	if (*(int*) ((char*) pObject + 0xa4) < 0x28) {
+		pMsg = 0;
+		pGroup = (void*) ((void* (__cdecl*)(int)) 0x45a780)(0x168);
+		if (pGroup != 0) {
+			pMsg = ((void* (__fastcall*)(void*, void*, void*, void*, void*)) 0x402103)(pGroup, 0,
+				(void*) (*(int*) 0x4a782c), (void*) (*(int*) 0x4a7830), (void*) (*(int*) 0x4a7834));
+		}
+		*(void**) ((char*) pObject + *(int*) ((char*) pObject + 0xa4) * 4 + 4) = pMsg;
+		*(int*) ((char*) pObject + 0xa4) += 1;
+		if (nIdCount != 0) {
+			unsigned int nIdx;
+			typedef void (__fastcall * GroupAddProc)(void*, void*, void*, void*);
+			GroupAddProc pAdd = *(GroupAddProc*) ((char*) *(void**) pObject + 0x3c);
+			for (nIdx = 0; nIdx < nIdCount; nIdx++) {
+				unsigned short uId = pIdList[nIdx];
+				pAdd(pObject, 0, (void*) (0x4a6510 + (unsigned int) uId * 4), pMsg);
+			}
+		}
+	}
+}
