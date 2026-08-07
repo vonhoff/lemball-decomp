521ed8297579eeb497c57abbc6e1aa907135d671
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-08-07 16:32:07 +0200

    rehome: reconstruct resize_registration_info_screen_viewport (0x44bc50, 43.6% - field-writes + conditional vtable pos-dispatch). Unc:68 of 528. lint clean

--- src/Control/ClusterScanOps.cpp
+++ src/Control/ClusterScanOps.cpp
@@ -201,6 +201,20 @@ void* __fastcall DeleteLinkedType11ChunkObject(void* pObject, int nUnused, unsig
 	return pObject;
 }
 
+// MACINTOSH: resize_registration_info_screen_viewport(short*)
+// FUNCTION: LEMBALL 0x0044bc50
+void __fastcall ResizeRegistrationInfoScreenViewport(void* pObject, int nUnused, short* param_1)
+{
+	*(short*) ((char*) pObject + 0x18) = param_1[0];
+	*(short*) ((char*) pObject + 0x1a) = param_1[1];
+	if (*(void**) ((char*) pObject + 0x8c) != 0) {
+		struct Pt { short x; short y; } pt;
+		pt.x = (short) ((param_1[0] - 0x60) / 2);
+		pt.y = (short) (param_1[1] - 0x20);
+		(*( void(**)(void*)) (*(void***) *(void**) ((char*) pObject + 0x8c) + 0x38 / 4))(&pt);
+	}
+}
+
 // MACINTOSH: get_timed_frame_sequence_index()
 // FUNCTION: LEMBALL 0x0044aa20
 unsigned int __fastcall GetTimedFrameSequenceIndex(void* pObject)
