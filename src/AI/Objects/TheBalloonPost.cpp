3ed4656905530f3f7d96f23aa1fff6c4080fe8cd
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-09-04 22:45:30 +0200

    Match balloon post construction

--- src/AI/Objects/TheBalloonPost.cpp
+++ src/AI/Objects/TheBalloonPost.cpp
@@ -1,9 +1,10 @@
 #include "TheBalloonPost.h"
 
 // 68K 0x106035ea __ct__15CTheBalloonPostF11eObjectTypeUc
-// STUB: LEMBALL 0x0042a5c0
-TheBalloonPost::TheBalloonPost(eObjectType p_arg0, unsigned char p_arg1)
+// FUNCTION: LEMBALL 0x0042a5c0
+TheBalloonPost::TheBalloonPost(eObjectType p_arg0, unsigned char p_arg1) : GameObject(p_arg0, 0, 10)
 {
+	m_active = *(unsigned int*) &p_arg1;
 }
 
 // 68K 0x101186cc __dt__15CTheBalloonPostFv
