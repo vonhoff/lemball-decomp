82b64986a45a3690f19aeb4f21330df0c8fecf14
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-09-04 22:53:26 +0200

    Match view data initialization

--- src/AI/Objects/ViewData.cpp
+++ src/AI/Objects/ViewData.cpp
@@ -1,7 +1,15 @@
 #include "ViewData.h"
 
 // 68K 0x1011cb4a __ct__9CViewDataFv
-// STUB: LEMBALL 0x00439340
+// FUNCTION: LEMBALL 0x00439340
 ViewData::ViewData()
 {
+	m_objectId = 0xffff;
+	m_auxiliaryPosition.m_xFixed = 0xaa55aa55;
+	m_auxiliaryPosition.m_yFixed = 0xaa55aa55;
+	m_auxiliaryPosition.m_zFixed = 0xaa55aa55;
+	m_transientFlags = 0;
+	m_unk0x30 = 0;
+	m_action = 0;
+	m_stateTimer = 0;
 }
