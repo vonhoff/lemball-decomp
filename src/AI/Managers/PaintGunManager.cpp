b7a537392b2c7118e564afcdebb443396a62dad5
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-09-05 01:26:39 +0200

    Reconstruct paint gun level parsing

--- src/AI/Managers/PaintGunManager.cpp
+++ src/AI/Managers/PaintGunManager.cpp
@@ -1,5 +1,6 @@
 #include "PaintGunManager.h"
 
+#include "../Navigation/Ai.h"
 #include "../Objects/PaintGun.h"
 
 // 68K 0x1061b8a8 __ct__16CPaintGunManagerFP3CAIi
@@ -61,9 +62,39 @@ void PaintGunManager::Add(unsigned short p_id, int p_x, int p_y, int p_z, int p_
 }
 
 // 68K 0x1061bcce LoadLevel__16CPaintGunManagerFPUciUc
-// STUB: LEMBALL 0x0042c610
+// FUNCTION: LEMBALL 0x0042c610
 void PaintGunManager::LoadLevel(unsigned char* p_data, int p_dataSize, unsigned char p_skip)
 {
+	unsigned short count = *(unsigned short*) p_data;
+	p_data += 2;
+	unsigned int remaining = count;
+	Initialise(remaining);
+	if (count != 0) {
+		unsigned short id;
+		unsigned short x;
+		unsigned short y;
+		unsigned short z;
+		unsigned short direction;
+		do {
+			if (m_ai->m_levelVersion > 1) {
+				id = *(unsigned short*) p_data;
+				p_data += 2;
+			}
+			else {
+				id = (unsigned short) GameObject::NextId();
+			}
+			x = *(unsigned short*) p_data;
+			p_data += 2;
+			y = *(unsigned short*) p_data;
+			p_data += 2;
+			z = *(unsigned short*) p_data;
+			p_data += 2;
+			direction = *(unsigned short*) p_data;
+			p_data += 2;
+			Add(id, x, y, z, direction);
+			remaining--;
+		} while (remaining != 0);
+	}
 }
 
 // 68K 0x1061ba6c __dt__16CPaintGunManagerFv
