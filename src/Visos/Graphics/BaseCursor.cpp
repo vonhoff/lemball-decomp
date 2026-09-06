beffd47915be85c47dde3c458d50647947f60094
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-09-06 22:47:44 +0200

    Handle cursor window sentinel positions

--- src/Visos/Graphics/BaseCursor.cpp
+++ src/Visos/Graphics/BaseCursor.cpp
@@ -277,16 +277,29 @@ bool BaseCursor::InWindow(GWnd* p_window)
 	short innerHeight;
 	short clipX;
 	short clipY;
+	VsPoint* innerXY;
 
 	width = p_window->m_rect.m_width;
 	height = p_window->m_rect.m_height;
-	x = p_window->m_rect.m_x;
-	y = p_window->m_rect.m_y;
+	if (p_window != (GWnd*) -8) {
+		x = p_window->m_rect.m_x;
+		y = p_window->m_rect.m_y;
+	}
+	else {
+		x = ((VsPoint*) 0)->m_x;
+		y = ((VsPoint*) 0)->m_y;
+	}
 	innerWidth = p_window->m_innerRect.m_width;
 	innerHeight = p_window->m_innerRect.m_height;
+	if (p_window != (GWnd*) -16) {
+		innerXY = (VsPoint*) &p_window->m_innerRect.m_x;
+	}
+	else {
+		innerXY = 0;
+	}
 	if ((int) innerHeight * (int) innerWidth != 0) {
-		clipX = (short) (p_window->m_innerRect.m_x + x);
-		clipY = (short) (p_window->m_innerRect.m_y + y);
+		clipX = (short) (innerXY->m_x + x);
+		clipY = (short) (innerXY->m_y + y);
 		if (x < clipX) {
 			width = (short) (width + (x - clipX));
 			x = clipX;
