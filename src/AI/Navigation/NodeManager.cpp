e3deac9ab96638bb1b4b086b4e758b22e45ac3ec
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-09-07 09:38:11 +0200

    Reconstruct NodeManager initialization

--- src/AI/Navigation/NodeManager.cpp
+++ src/AI/Navigation/NodeManager.cpp
@@ -2,6 +2,8 @@
 
 #include "Node.h"
 
+#include <new.h>
+
 // 68K 0x106184ca __ct__12CNodeManagerFi
 // FUNCTION: LEMBALL 0x00421180
 NodeManager::NodeManager(int p_arg0)
@@ -24,9 +26,39 @@ void NodeManager::Restart()
 }
 
 // 68K 0x1061854c Initialise__12CNodeManagerFi
-// STUB: LEMBALL 0x004211d0
+// FUNCTION: LEMBALL 0x004211d0
 void NodeManager::Initialise(int p_count)
 {
+	m_count = 0;
+	if (p_count != 0) {
+		if (m_nodes == 0) {
+			int* allocation = (int*) operator new(m_capacity * sizeof(Node) + sizeof(int));
+			if (allocation == 0) {
+				m_nodes = 0;
+			}
+			else {
+				Node* nodes = (Node*) (allocation + 1);
+				*allocation = m_capacity;
+				int i = m_capacity;
+				while (i > 0) {
+					i--;
+					new ((void*) nodes) Node();
+					nodes++;
+				}
+				m_nodes = (Node*) (allocation + 1);
+			}
+		}
+
+		int i = 0;
+		if (m_capacity > 0) {
+			do {
+				m_nodes[i].Restart();
+				i++;
+			} while (i < m_capacity);
+		}
+		return;
+	}
+	m_nodes = 0;
 }
 
 // 68K 0x106185ee __dt__12CNodeManagerFv
