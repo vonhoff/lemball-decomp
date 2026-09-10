c1de07a45aade0ad0c0fd227c3212f94cc4c74ba
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-09-10 12:07:37 +0200

    Consolidate GroundArray translation unit

--- src/Map/Ground/GroundArray.cpp
+++ src/Map/Ground/GroundArray.cpp
@@ -2,6 +2,12 @@
 
 #include "Ground.h"
 
+// FUNCTION: LEMBALL 0x0040eb70
+Ground* GroundArray::GetGroundCell(int p_x, int p_y)
+{
+	return &m_ground[m_width * p_y + p_x];
+}
+
 // 68K 0x10119a8e SetCollision__12CGroundArrayFiii
 // FUNCTION: LEMBALL 0x0040eb90
 void GroundArray::SetCollision(int p_x, int p_y, int p_collision)
--- src/Map/Ground/GroundArrayGetGroundCell.cpp
+++
@@ -1,7 +0,0 @@
-#include "GroundArray.h"
-
-// FUNCTION: LEMBALL 0x0040eb70
-Ground* GroundArray::GetGroundCell(int p_x, int p_y)
-{
-	return &m_ground[m_width * p_y + p_x];
-}
