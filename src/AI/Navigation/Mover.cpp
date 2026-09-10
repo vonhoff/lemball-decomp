3eecb679eea871fcd061c3fb070bfc7d223029e2
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-09-10 12:02:55 +0200

    Consolidate mover translation units

--- src/AI/Navigation/Mover.cpp
+++ src/AI/Navigation/Mover.cpp
@@ -40,6 +40,21 @@ void Mover::SetPos()
 {
 }
 
+// 68K 0x106173a0 IsAt__6CMoverFiiRi
+// FUNCTION: LEMBALL 0x0042e700
+bool Mover::IsAt(int p_x, int p_y, int& p_height)
+{
+	int x = (m_position.m_xFixed >> 12) - 8;
+	int xMax = x + 15;
+	int y = (m_position.m_yFixed >> 12) - 8;
+	int yMax = y + 15;
+	if (p_x >= x && p_x <= xMax && p_y >= y && p_y <= yMax) {
+		p_height = (m_position.m_zFixed >> 12) + 8;
+		return true;
+	}
+	return false;
+}
+
 // 68K 0x10617410 Set__6CMoverFUsiUcii
 // FUNCTION: LEMBALL 0x0042e760
 void Mover::Set(unsigned short p_id, int p_pathId, undefined4 p_movementMode, int p_startNode, int p_nodeCount)
--- src/AI/Navigation/MoverManager.cpp
+++ src/AI/Navigation/MoverManager.cpp
@@ -50,6 +50,17 @@ MoverManager::~MoverManager()
 }
 
 // 68K 0x10618140 Find__13CMoverManagerFiiRi
+// FUNCTION: LEMBALL 0x0042f2f0
+Mover* MoverManager::Find(int p_x, int p_y, int& p_height)
+{
+	for (int i = 0; i < m_count; i++) {
+		if (m_movers[i].IsAt(p_x, p_y, p_height)) {
+			return &m_movers[i];
+		}
+	}
+	return 0;
+}
+
 // 68K 0x106181c2 Process__13CMoverManagerFv
 // FUNCTION: LEMBALL 0x0042f500
 void MoverManager::Process()
@@ -75,6 +86,23 @@ void MoverManager::Add(unsigned short p_id, int p_pathId, undefined4 p_movementM
 	if (m_count < m_capacity) {
 		m_movers[m_count].Set(p_id, p_pathId, p_movementMode, p_startNode, p_nodeCount);
 		m_count++;
+	}
+}
+
+// 68K 0x10618322 Switch__13CMoverManagerF9swMessagei
+// FUNCTION: LEMBALL 0x0042f620
+void MoverManager::Switch(int p_message, int p_id)
+{
+	MoverManager* self = this;
+	int index = 0;
+	while (index < self->m_count) {
+		if ((unsigned short) self->m_movers[index].GetId() == p_id) {
+			if (p_message == 4) {
+				self->m_movers[index].Switch();
+			}
+			return;
+		}
+		index++;
 	}
 }
 
