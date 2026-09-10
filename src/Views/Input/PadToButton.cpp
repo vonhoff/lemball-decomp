7f2f91ae5bdf47eada9844f29e89962815b02363
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-09-10 20:31:42 +0200

    Match pad input mapping construction

--- src/Views/Input/PadToButton.cpp
+++ src/Views/Input/PadToButton.cpp
@@ -1,9 +1,19 @@
 #include "PadToButton.h"
 
+#include "../../Visos/Foundation/BaseQueue.h"
+#include "PadToButtonEntry.h"
+
 // 68K 0x10b0f952 __ct__12CPadToButtonFi
-// STUB: LEMBALL 0x0043a250
+// FUNCTION: LEMBALL 0x0043a250
 PadToButton::PadToButton(int p_arg0)
 {
+	m_entries = new PadToButtonEntry[p_arg0];
+	m_entryCapacity = p_arg0;
+	m_entryCount = 0;
+	for (int i = 0; i < p_arg0; i++) {
+		m_entries[i].m_button = 0;
+	}
+	g_pMasterInputQueue->Attach(this, -25);
 }
 
 // 68K 0x10b0fa5c ProcessMsg__12CPadToButtonFP10tagMESSAGE
