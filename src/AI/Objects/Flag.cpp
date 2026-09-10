d74bdbfdd1207b435291e0e8903d3022c5d35af0
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-09-10 21:52:02 +0200

    Match flag collection

--- src/AI/Objects/Flag.cpp
+++ src/AI/Objects/Flag.cpp
@@ -1,5 +1,7 @@
 #include "Flag.h"
 
+#include "../Navigation/Ai.h"
+
 // 68K 0x10605e54 SetSFX__5CFlagFv
 // FUNCTION: LEMBALL 0x00422b30
 void Flag::SetSfx()
@@ -15,10 +17,13 @@ bool Flag::Process()
 }
 
 // 68K 0x10605f2e Collected__5CFlagFv
-// STUB: LEMBALL 0x00422c00
+// FUNCTION: LEMBALL 0x00422c00
 int Flag::Collected()
 {
-	return 0;
+	m_activator->AddObject(m_objectType, this);
+	g_pAI->Score(2500);
+	g_pAI->m_flagCounts[0]--;
+	return 1;
 }
 
 // 68K 0x10119982 __dt__5CFlagFv
