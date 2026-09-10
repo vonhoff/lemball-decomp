f645f840489726b630ddefe08c728ab300b1344f
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-09-10 18:52:34 +0200

    Complete PvButton owner constructor

--- src/Visos/Graphics/PvButton.cpp
+++ src/Visos/Graphics/PvButton.cpp
@@ -83,11 +83,9 @@ PvButton::PvButton(const VsRect& p_arg0, PvGWnd* p_arg1)
 }
 
 // 68K 0x1020fa78 __ct__9CPVButtonFP7CPVGWnd
-// STUB: LEMBALL 0x00467cd0
-PvButton::PvButton(PvGWnd* p_arg0)
+// FUNCTION: LEMBALL 0x00467cd0
+PvButton::PvButton(PvGWnd* p_arg0) : m_buttonY(0), m_buttonX(0)
 {
-	m_buttonY = 0;
-	m_buttonX = 0;
 	m_clickY = 0;
 	m_clickX = 0;
 	m_ownerWindow = p_arg0;
