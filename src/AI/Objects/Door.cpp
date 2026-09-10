8638b6fb791c64d162277eb9b00da797033d6cde
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-09-10 12:05:59 +0200

    Consolidate Door translation unit

--- src/AI/Objects/Door.cpp
+++ src/AI/Objects/Door.cpp
@@ -3,6 +3,7 @@
 #include "../../Control/Game/Game.h"
 #include "../../Control/Game/GameTime.h"
 #include "../../Map/Base/Map.h"
+#include "../Navigation/Ai.h"
 
 // 68K 0x106062b0 __ct__5CDoorFv
 // FUNCTION: LEMBALL 0x0040d470
@@ -211,6 +212,32 @@ int Door::Hits(const AiCoord& p_position, GameObject* p_object)
 }
 
 // 68K 0x10606be8 DoActivate__5CDoorFv
+// FUNCTION: LEMBALL 0x0040dec0
+void Door::DoActivate()
+{
+	m_activationPending = 1;
+	m_stateTimer = g_dwSimulationTimestamp;
+	m_actionDeadline += g_dwGameTick;
+	if (m_action != (eAction) 0x1c) {
+		int actionArgument = (unsigned short) m_actionArgument;
+		int score;
+		switch (actionArgument) {
+		case 0x14:
+			score = 0x19;
+			break;
+		case 0x15:
+		case 0x16:
+		case 0x17:
+			score = 0x4b;
+			break;
+		default:
+			score = 0x19;
+			break;
+		}
+		g_pAI->Score(score);
+	}
+}
+
 // 68K 0x10119b74 __dt__5CDoorFv
 Door::~Door()
 {
