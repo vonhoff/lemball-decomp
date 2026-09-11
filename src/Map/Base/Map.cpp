dcf49b5cadb3db08b900b41d8871bc78f42a2f74
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-09-11 17:56:06 +0200

    Reconstruct in-place map projection

--- src/Map/Base/Map.cpp
+++ src/Map/Base/Map.cpp
@@ -358,9 +358,29 @@ void Map::GameToScreen(int p_gameX, int p_gameY, int& p_screenX, int& p_screenY)
 }
 
 // 68K 0x10900c34 GameToScreen__4CMapFRiRi
-// STUB: LEMBALL 0x00430ce0
+// FUNCTION: LEMBALL 0x00430ce0
 void Map::GameToScreen(int& p_x, int& p_y)
 {
+	int* outputY = &p_y;
+	int y = *outputY;
+	int x = p_x;
+	switch (m_orientation) {
+	case 0:
+		p_x = x - y + 0x10;
+		*outputY = y / 2 + x / 2;
+		break;
+	case 1:
+		p_x = 0x20 - y - x;
+		*outputY = x / 2 - y / 2 + 8;
+		break;
+	case 2:
+		p_x = y - x + 0x10;
+		*outputY = 0x10 - y / 2 - x / 2;
+		break;
+	case 3:
+		p_x = x + y;
+		*outputY = y / 2 - x / 2 + 8;
+	}
 }
 
 // 68K 0x10900d48 LoadLevel__4CMapFP24tagLoadGroundSurfaceDataUlUc
