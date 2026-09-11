34f8e64faa915af725b9c032aa4649ed7451f725
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-09-12 01:35:44 +0200

    Reconstruct broadcast setup string sequencing

--- src/Visos/Network/FileBroadcast.cpp
+++ src/Visos/Network/FileBroadcast.cpp
@@ -14,7 +14,7 @@
 #include <new.h>
 #include <string.h>
 
-#pragma intrinsic(memcpy, strcpy, strlen)
+#pragma intrinsic(memcpy, strcpy, strlen, strcat)
 
 extern "C" unsigned long __stdcall timeGetTime(void);
 
@@ -22,7 +22,6 @@ extern "C" unsigned long __stdcall timeGetTime(void);
 // FUNCTION: LEMBALL 0x0046f4f0
 void FileBroadcast::Setup(const char* p_peerName, const char* p_path)
 {
-	char port[2];
 	unsigned int length;
 
 	g_szBroadcastPeerName = (char*) operator new(strlen(p_peerName) + 1);
@@ -31,16 +30,17 @@ void FileBroadcast::Setup(const char* p_peerName, const char* p_path)
 	g_unk0x4a1e44 = 1;
 
 	g_pFileBroadcastData = (char*) operator new(strlen(p_path) + 0xf);
+	length = strlen(p_path);
 	strcpy(g_pFileBroadcastData, p_path);
-	length = strlen(g_pFileBroadcastData);
-	if (g_pFileBroadcastData[length - 1] != '\\' && g_pFileBroadcastData[length - 1] != ':') {
-		memcpy(g_pFileBroadcastData + length, "\\", 2);
+	if (g_pFileBroadcastData[length] != '\\' && g_pFileBroadcastData[length] != ':') {
+		memcpy(g_pFileBroadcastData + strlen(g_pFileBroadcastData), "\\", 2);
 	}
+	char port[2] = "0";
 	memcpy(g_pFileBroadcastData + strlen(g_pFileBroadcastData), "VSNETv", 7);
 	VsLtoa(0, port, 10);
-	memcpy(g_pFileBroadcastData + strlen(g_pFileBroadcastData), port, strlen(port) + 1);
+	strcat(g_pFileBroadcastData, port);
 	VsLtoa(9, port, 10);
-	memcpy(g_pFileBroadcastData + strlen(g_pFileBroadcastData), port, strlen(port) + 1);
+	strcat(g_pFileBroadcastData, port);
 }
 
 // 68K 0x10208278 __ct__14CFileBroadcastFv
