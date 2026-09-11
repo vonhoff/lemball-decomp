0e66a730d506e530e1722c8cc8d0f34060fb25af
Simon Vonhoff <simon.vonhoff@outlook.com>
2026-09-11 03:41:20 +0200

    Reconstruct maze search initialization

--- src/AI/Navigation/Maze.cpp
+++ src/AI/Navigation/Maze.cpp
@@ -4,6 +4,8 @@
 
 #include <string.h>
 
+extern "C" unsigned long __stdcall timeGetTime(void);
+
 extern const int g_unk0x00495b30[10];
 extern const int g_unk0x00495b58[10];
 extern const int g_unk0x00495b80[10];
@@ -256,9 +258,58 @@ void Maze::SwapChange()
 }
 
 // 68K 0x10615e5a BInitialise__5CMazeFUciiii
-// STUB: LEMBALL 0x00423530
+// FUNCTION: LEMBALL 0x00423530
 void Maze::BInitialise(unsigned char p_resetStats, int p_startX, int p_startY, int p_endX, int p_endY)
 {
+	if (p_resetStats != 0) {
+		m_totalTime = 0;
+		m_solutionCount = 0;
+	}
+	m_startTime = timeGetTime();
+	m_radius = 0;
+	m_startX = p_startX;
+	m_startY = p_startY;
+	m_endX = p_endX;
+	m_endY = p_endY;
+
+	int y = 0;
+	if (m_height > 0) {
+		do {
+			int x = 0;
+			if (m_width > 0) {
+				do {
+					unsigned short collision;
+					if (x < 0 || y < 0) {
+						collision = 3;
+					}
+					else {
+						Map* map = m_map;
+						int width = map->m_ground.m_width;
+						if (width <= x || map->m_ground.m_height <= y) {
+							collision = 3;
+						}
+						else {
+							collision = map->m_ground.m_ground[y * width + x].m_collision;
+						}
+					}
+					if ((collision & 1) != 0) {
+						m_distances[y][x] = 0xffff;
+					}
+					else {
+						m_distances[y][x] = 0xff00;
+					}
+					x++;
+				} while (x < m_width);
+			}
+			y++;
+		} while (y < m_height);
+	}
+
+	m_changeSelect = 0;
+	Clear(m_changeA);
+	Clear(m_changeB);
+	UpdateChangeNext(m_startX, m_startY);
+	m_distances[m_startY][m_startX] = 0;
 }
 
 // 68K 0x10615f52 BIteration__5CMazeFRUcRUc
