672e3394545102db634fe2a31b039242b346da45
Simon Vonhoff <55744345+vonhoff@users.noreply.github.com>
2026-09-12 21:17:12 +0200

    Match inlined point initialization order

--- src/Visos/Foundation/VsPoint.h
+++ src/Visos/Foundation/VsPoint.h
@@ -8,7 +8,7 @@ struct VsPoint {
 	VsPoint(const VsPoint& p_source) : m_x(p_source.m_x), m_y(p_source.m_y) {}
 
 	// FUNCTION: LEMBALL 0x0044b5d0
-	VsPoint() : m_x(0), m_y(0) {}
+	VsPoint() { m_x = m_y = 0; }
 
 	VsPoint(short p_x, short p_y) : m_x(p_x), m_y(p_y) {}
 
