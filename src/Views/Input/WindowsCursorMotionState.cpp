088a7609be903d6436c70b611394b3c13ee876b2
Simon Vonhoff <55744345+vonhoff@users.noreply.github.com>
2026-09-14 18:10:43 +0200

    Match cursor position and auxiliary state setters

--- src/Views/Input/WindowsCursorMotionState.cpp
+++ src/Views/Input/WindowsCursorMotionState.cpp
@@ -2,8 +2,25 @@
 
 #include "../../Visos/Foundation/VsPoint.h"
 
+// FUNCTION: LEMBALL 0x00432810
+void WindowsCursorMotionState::SetCursorMotionPoint(const VsPoint& p_position)
+{
+	m_positionDirty = 1;
+	m_horizontalActive = 0;
+	m_verticalActive = 0;
+	m_fixedY = p_position.m_y << 12;
+	m_fixedX = p_position.m_x << 12;
+}
+
 // FUNCTION: LEMBALL 0x00432840
 VsPoint WindowsCursorMotionState::GetCursorMotionPoint()
 {
 	return VsPoint((short) (m_fixedX >> 12), (short) (m_fixedY >> 12));
+}
+
+// FUNCTION: LEMBALL 0x00432920
+void WindowsCursorMotionState::SetCursorMotionAuxValues(unsigned int p_aux0, unsigned int p_aux1)
+{
+	m_aux0 = p_aux0;
+	m_aux1 = p_aux1;
 }
--- src/Views/Input/WindowsCursorMotionState.h
+++ src/Views/Input/WindowsCursorMotionState.h
@@ -8,7 +8,9 @@
 // SIZE 0x78
 class WindowsCursorMotionState {
 public:
+	void SetCursorMotionPoint(const VsPoint& p_position);
 	VsPoint GetCursorMotionPoint();
+	void SetCursorMotionAuxValues(unsigned int p_aux0, unsigned int p_aux1);
 
 private:
 	undefined4 m_unknown00;          // 0x00
