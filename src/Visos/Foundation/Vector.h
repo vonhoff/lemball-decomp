7c9263f4e182d01d2f1d0e826548c39f57d49b39
Simon Vonhoff <55744345+vonhoff@users.noreply.github.com>
2026-09-15 10:56:37 +0200

    Map the original vector copy constructor

--- src/Visos/Foundation/Vector.h
+++ src/Visos/Foundation/Vector.h
@@ -9,6 +9,7 @@ class Vector {
 public:
 	Vector();
 	inline Vector(int p_x, int p_y) : m_xFixed(p_x), m_yFixed(p_y) {}
+	// FUNCTION: LEMBALL 0x00417b30
 	inline Vector(const Vector& p_other) : m_xFixed(p_other.m_xFixed), m_yFixed(p_other.m_yFixed) {}
 	Vector* SetIntegers(int p_x, int p_y);
 
