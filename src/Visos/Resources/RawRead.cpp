6817b6086adfc2190955856dc7ad2b8a3e82ff1a
Simon Vonhoff <55744345+vonhoff@users.noreply.github.com>
2026-09-22 13:33:32 +0200

    Match raw resource name comparison register allocation

--- src/Visos/Resources/RawRead.cpp
+++ src/Visos/Resources/RawRead.cpp
@@ -39,9 +39,9 @@ unsigned int RawRead::InputDword()
 // FUNCTION: LEMBALL 0x0045bd50
 bool RawRead::NameCmp(char* p_path, char* p_name)
 {
+	char* name = p_name;
 	int matched = 0;
 	char* path = p_path;
-	char* name = p_name;
 	do {
 		char pathChar = (char) toupper(*path);
 		if (pathChar == kPathSeparator) {
