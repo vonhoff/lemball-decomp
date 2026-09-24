1d53dfd25080c6d29ffe613cf6776b55f01a3db4
Simon Vonhoff <55744345+vonhoff@users.noreply.github.com>
2026-09-24 16:26:23 +0200

    Isolate matching mover position update

--- src/AI/Navigation/CMover.cpp
+++ src/AI/Navigation/CMover.cpp
@@ -48,28 +48,6 @@ CMover::~CMover()
 {
 }
 
-// FUNCTION: LEMBALL 0x0042e650
-void CMover::SetPos()
-{
-	int x = (m_position.m_xFixed >> 12) - 8;
-	int maxX = x + 15;
-	int y = (m_position.m_yFixed >> 12) - 8;
-	int maxY = y + 15;
-	x /= 16;
-	y /= 16;
-	maxX /= 16;
-	maxY /= 16;
-	for (int groundY = y; groundY <= maxY; groundY++) {
-		for (int groundX = x; groundX <= maxX; groundX++) {
-			if (groundX >= 0 && groundY >= 0 && groundX < g_pMap->m_ground.m_width &&
-				groundY < g_pMap->m_ground.m_height) {
-				CGround* ground = &g_pMap->m_ground.m_ground[groundY * g_pMap->m_ground.m_width + groundX];
-				ground->m_collision |= 0x10;
-			}
-		}
-	}
-}
-
 // FUNCTION: LEMBALL 0x0042e700
 bool CMover::IsAt(int p_x, int p_y, int& p_height)
 {
