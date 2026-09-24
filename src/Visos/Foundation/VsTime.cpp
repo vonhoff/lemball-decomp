aa452de63347cf0e0c66739d6e3df5c09791c8be
Simon Vonhoff <55744345+vonhoff@users.noreply.github.com>
2026-09-24 06:40:09 +0200

    Match WinMM timer import jump form

--- src/Visos/Foundation/VsTime.cpp
+++ src/Visos/Foundation/VsTime.cpp
@@ -1,6 +1,6 @@
 #include "VsTime.h"
 
-extern "C" unsigned long __stdcall timeGetTime(void);
+extern "C" __declspec(dllimport) unsigned long __stdcall timeGetTime(void);
 
 // FUNCTION: LEMBALL 0x00462e80
 unsigned long CurrentMilliTimer()
