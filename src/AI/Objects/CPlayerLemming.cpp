347b7493002c93e37530d28b6487f990404727b9
Simon Vonhoff <55744345+vonhoff@users.noreply.github.com>
2026-09-25 18:09:06 +0200

    Match CPlayerLemming constructor assignment order

--- src/AI/Objects/CPlayerLemming.cpp
+++ src/AI/Objects/CPlayerLemming.cpp
@@ -43,9 +43,9 @@ CPlayerLemming::CPlayerLemming(int p_x,
 	m_alternatePlayer = p_alternatePlayer;
 	m_spawnPosition.m_xFixed = p_x << 12;
 	m_spawnPosition.m_yFixed = p_y << 12;
+	m_spawnPosition.m_zFixed = p_z << 12;
 	m_spawnDelay = p_spawnDelay;
 	m_initialFacingDirection = (short) p_facing;
-	m_spawnPosition.m_zFixed = p_z << 12;
 	if (p_alternatePlayer != 0) {
 		SetId(NextLoadingId());
 	}
