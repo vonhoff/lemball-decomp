7f0342096fbbd6213015343a80673aa30a15c48d
Simon Vonhoff <55744345+vonhoff@users.noreply.github.com>
2026-09-25 13:22:38 +0200

    Annotate verified CLoadUpdate construction vtable

--- src/Views/Sound/CLoadUpdate.h
+++ src/Views/Sound/CLoadUpdate.h
@@ -1,6 +1,7 @@
 #ifndef LEMBALL_VIEWS_SOUND_CLOADUPDATE_H
 #define LEMBALL_VIEWS_SOUND_CLOADUPDATE_H
 
+// VTABLE: LEMBALL 0x00496cd8
 class CLoadUpdate {
 public:
 	virtual void UpdateNonCacheLoad() = 0; // vtable+0x00
