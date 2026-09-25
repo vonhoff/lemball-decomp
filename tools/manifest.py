2b5b02b083208066c3bf83aa71cfdd997b88e13b
Simon Vonhoff <55744345+vonhoff@users.noreply.github.com>
2026-09-25 11:04:33 +0200

    Restore reproducible resource manifest generator

---
+++ tools/manifest.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Regenerate the resource manifest from data/pbaimog.vsr."""
+
+from lib.manifest import generate_manifest
+
+
+if __name__ == "__main__":
+    raise SystemExit(generate_manifest())
